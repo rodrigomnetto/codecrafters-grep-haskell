fa27506a4d0db4ddaba15ef1967996fd3cffae53
Netto <rodrigomnto@gmail.com>
2025-04-18 18:48:27 -0300

    enhancing readability

--- app/Main.hs
+++ app/Main.hs
@@ -6,33 +6,33 @@ import Data.Char
 import System.IO (hSetBuffering, stdout, stderr, BufferMode (NoBuffering))
 
 matchPattern :: String -> String -> Bool
-matchPattern pattern input = do
+matchPattern pattern input =
   if length pattern == 1
     then head pattern `elem` input
-    else 
-      let (f, _) = (parsePattern pattern) in
-        f (head input)
+    else
+      let (f, _) = parsePattern pattern
+      in f (head input)
 
 parsePattern :: String -> (Char -> Bool, String)
 parsePattern pattern =
   case pattern of
-    '\\':r1   -> case r1 of
-      'd':r2 -> (isDigit, r2)
-      'w':r2 -> (isDigitOrLetter, r2)
-    '[':r1    -> isAny r1
-    
+    '\\' : r1 -> case r1 of
+      'd' : r2 -> (isDigit, r2)
+      'w' : r2 -> (isDigitOrLetter, r2)
+    '[' : r1  -> isAny r1
+
 isChar :: Char -> Char -> Bool
-isChar chr c = (chr == c)
+isChar chr c = chr == c
 
 isAny :: String -> (Char -> Bool, String)
-isAny [] = (\_ -> False, [])
-isAny (']':xs) = (\_ -> False, xs)
-isAny (x:xs)   = 
-  let (f1, r) = isAny xs 
+isAny []         = (\_ -> False, [])
+isAny (']' : xs) = (\_ -> False, xs)
+isAny (x : xs)   =
+  let (f1, r) = isAny xs
   in (\y -> isChar x y || f1 y, r)
 
 isDigitOrLetter :: Char -> Bool
-isDigitOrLetter c = (isDigit c) || (isAlpha c)
+isDigitOrLetter c = isDigit c || isAlpha c
 
 main :: IO ()
 main = do
@@ -44,11 +44,12 @@ main = do
   let pattern = args !! 1
   input_line <- getLine
 
-  -- Uncomment this block to pass stage 1
+  -- Check for -E flag
   if head args /= "-E"
     then do
       putStrLn "Expected first argument to be '-E'"
       exitFailure
-    else do if matchPattern pattern input_line
-              then exitSuccess
-              else exitFailure
+    else
+      if matchPattern pattern input_line
+        then exitSuccess
+        else exitFailure

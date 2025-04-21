9fd9423dd4477303a1ed56d5fda08f81293cd2d4
Netto <rodrigomnto@gmail.com>
2025-04-21 19:19:13 -0300

    refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,7 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use tuple-section" #-}
+{-# HLINT ignore "Redundant lambda" #-}
+
 module Main where
 
 import System.Environment
@@ -5,41 +9,69 @@ import System.Exit
 import Data.Char
 import System.IO (hSetBuffering, stdout, stderr, BufferMode (NoBuffering))
 
-matchPattern :: String -> String -> Bool
-matchPattern [] _ = True
-matchPattern (_:_) [] = False
-matchPattern pattern input =
-  let (f, ptr) = parsePattern pattern in
-    if f (head input) == True
-      then matchPattern ptr (tail input)
-      else matchPattern pattern (tail input)
 
-parsePattern :: String -> (Char -> Bool, String)
-parsePattern pattern =
-  case pattern of
-    '\\' : r1 -> case r1 of
-      'd' : r2 -> (isDigit, r2)
-      'w' : r2 -> (isDigitOrLetter, r2)
-    '[' : r1  -> case r1 of
-      '^' : r2 -> 
-        let (f1, r3) = isAny r2
-        in (not . f1, r3)
-      _        ->
-        isAny r1
-    x   : xs -> (isChar x, xs)
-      
-isChar :: Char -> Char -> Bool
-isChar chr c = chr == c
+type Regex = (String -> (Bool, String))
 
-isAny :: String -> (Char -> Bool, String)
-isAny []         = (\_ -> False, [])
-isAny (']' : xs) = (\_ -> False, xs)
-isAny (x : xs)   =
-  let (f1, r) = isAny xs
-  in (\y -> isChar x y || f1 y, r)
+matchPattern :: String -> Regex -> Bool
+matchPattern [] _ = False
+matchPattern input regex =
+  let (r, rest) = regex input in
+    if r
+      then r
+      else matchPattern rest regex
 
-isDigitOrLetter :: Char -> Bool
-isDigitOrLetter c = isDigit c || isAlpha c
+parseRegex :: String -> Regex
+parseRegex [] = \x -> (True, x)
+parseRegex pattern =
+  let (f, rest) = case pattern of
+        '\\': r1 -> case r1 of
+          'd' : r2 -> (isDigit2, r2)
+          'w' : r2 -> (isDigitOrLetter, r2)
+        '[' : r1  -> case r1 of
+          '^' : r2 -> isNotAny r2
+          _        -> isAny r1
+        x   : xs -> (isChar x, xs)
+  in
+    f `andThen` parseRegex rest
+
+andThen :: Regex -> Regex -> Regex
+andThen f1 f2 =
+  \x -> let (r1, rest1) = f1 x in
+    if r1
+      then f2 rest1
+      else (r1, rest1)
+
+orElse :: Regex -> Regex -> Regex
+orElse f1 f2 =
+  \x -> let (r1, rest1) = f1 x in
+    if r1
+      then (r1, rest1)
+      else f2 x
+
+isDigit2 :: Regex
+isDigit2 [] = (False, [])
+isDigit2 (x:xs) = (isDigit x, xs)
+
+isChar :: Char -> Regex
+isChar chr [] = (False, [])
+isChar chr (x:xs) = (x == chr, xs)
+
+isNotAny :: String -> (Regex, String)
+isNotAny pattern =
+  let (rgx, ptr) = isAny pattern
+  in (\input ->
+        let (r, rest) = rgx input
+        in (not r, rest), ptr)
+
+isAny :: String -> (Regex, String)
+isAny (']' : xs) = (\x -> (False, x), xs)
+isAny (x:xs) =
+  let (p, r) = isAny xs
+  in (isChar x `orElse` p , r)
+
+isDigitOrLetter :: Regex
+isDigitOrLetter [] = (False, [])
+isDigitOrLetter (x:xs) = (isDigit x || isAlpha x, xs)
 
 main :: IO ()
 main = do
@@ -51,12 +83,12 @@ main = do
   let pattern = args !! 1
   input_line <- getLine
 
-  -- Check for -E flag
   if head args /= "-E"
     then do
       putStrLn "Expected first argument to be '-E'"
       exitFailure
     else
-      if matchPattern pattern input_line
-        then exitSuccess
-        else exitFailure
+      let regex = parseRegex pattern in
+        if matchPattern input_line regex
+          then exitSuccess
+          else exitFailure

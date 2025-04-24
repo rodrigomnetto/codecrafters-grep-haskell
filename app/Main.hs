b312550f8ae81a60693d0c9de5e4757e1f83ebe8
Netto <rodrigomnto@gmail.com>
2025-04-24 00:40:20 -0300

    add anchors

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,17 @@ import System.Environment
 import System.Exit
 import Data.Char
 import System.IO (hSetBuffering, stdout, stderr, BufferMode (NoBuffering))
+import GHC.Generics (Meta)
 
 
-type Regex = (String -> (Bool, String))
+type Metadata = (Int, [(Int, Char)]);
+type Regex = (Metadata -> (Bool, Metadata))
 
-matchPattern :: String -> Regex -> Bool
-matchPattern [] _ = False
-matchPattern input regex =
-  let (r, rest) = regex input in
+matchPattern :: Metadata -> Regex -> Bool
+matchPattern metadata regex
+  | isEmpty metadata = False
+  | otherwise = 
+    let (r, rest) = regex metadata in
     if r
       then r
       else matchPattern rest regex
@@ -24,6 +27,8 @@ parseRegex :: String -> Regex
 parseRegex [] = \x -> (True, x)
 parseRegex pattern =
   let (f, rest) = case pattern of
+        '^' : r1 -> (startAnchor, r1)
+        '$' : r1 -> (endAnchor, r1)
         '\\': r1 -> case r1 of
           'd' : r2 -> (isDigit2, r2)
           'w' : r2 -> (isDigitOrLetter, r2)
@@ -48,14 +53,35 @@ orElse f1 f2 =
       then (r1, rest1)
       else f2 x
 
+startAnchor :: Regex
+startAnchor metadata = 
+  let isBegin = getIndex metadata == 0 in
+    if isBegin
+    then (True, metadata)
+    else (False, clean metadata)
+
+endAnchor :: Regex
+endAnchor metadata = (isEmpty metadata, metadata)
+
 isDigit2 :: Regex
-isDigit2 [] = (False, [])
-isDigit2 (x:xs) = (isDigit x, xs)
+isDigit2 metadata 
+  | isEmpty metadata = (False, metadata)
+  | otherwise =
+    let chr = getCharacter metadata
+        tailMetadata = getTail metadata
+    in
+      (isDigit chr, tailMetadata)
 
 isChar :: Char -> Regex
-isChar chr [] = (False, [])
-isChar chr (x:xs) = (x == chr, xs)
+isChar char metadata
+  | isEmpty metadata = (False, metadata)
+  | otherwise = 
+    let chr = getCharacter metadata
+        tailMetadata = getTail metadata
+    in
+      (char == chr, tailMetadata)
 
+--improve
 isNotAny :: String -> (Regex, String)
 isNotAny pattern =
   let (rgx, ptr) = isAny pattern
@@ -64,14 +90,48 @@ isNotAny pattern =
         in (not r, rest), ptr)
 
 isAny :: String -> (Regex, String)
-isAny (']' : xs) = (\x -> (False, x), xs)
+isAny (']' : xs) = (\x -> (False, getTail x), xs)
 isAny (x:xs) =
   let (p, r) = isAny xs
   in (isChar x `orElse` p , r)
 
 isDigitOrLetter :: Regex
-isDigitOrLetter [] = (False, [])
-isDigitOrLetter (x:xs) = (isDigit x || isAlpha x, xs)
+isDigitOrLetter metadata 
+  | isEmpty metadata = (False, metadata)
+  | otherwise = 
+    let chr = getCharacter metadata
+        tailMetadata = getTail metadata
+    in
+      (isDigit chr || isAlpha chr, tailMetadata)
+
+--metadata functions
+
+extractMetadata :: String -> Metadata
+extractMetadata input = 
+  let size = length input 
+  in
+    (size, zip [0..size - 1]  input) 
+
+isEmpty :: Metadata -> Bool
+isEmpty (_, []) = True
+isEmpty _ = False
+
+getCharacter :: Metadata -> Char
+getCharacter (_, (_,c):_) = c
+
+getIndex :: Metadata -> Int
+getIndex (_, (i,_):_) = i
+
+getSize :: Metadata -> Int --maybe its not necessary
+getSize (s, _) = s
+
+getTail :: Metadata -> Metadata
+getTail (s, x:xs) = (s, xs)
+
+clean :: Metadata -> Metadata
+clean (s, _) = (s, [])
+
+--end of metadata functions
 
 main :: IO ()
 main = do
@@ -88,7 +148,9 @@ main = do
       putStrLn "Expected first argument to be '-E'"
       exitFailure
     else
-      let regex = parseRegex pattern in
-        if matchPattern input_line regex
+      let regex = parseRegex pattern
+          metadata = extractMetadata input_line 
+      in
+        if matchPattern metadata regex
           then exitSuccess
           else exitFailure
